bb632e38d58c772a6562096353f5de09136d5f80
Siarhei Lunski <loonskai@gmail.com>
2020-11-25 18:38:55 +0300

    Setup project

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,11 @@
 import * as Phraser from 'phaser';
-import { LoadScene } from './scenes/LoadScene';
-import { MenuScene } from './scenes/MenuScene';
-import { HUDScene } from './scenes/HUDScene';
-import { GameScene } from './scenes/GameScene';
 
 const gameConfig: Phaser.Types.Core.GameConfig = {
   title: 'Run at Full Speed',
   width: 640,
   height: 360,
   backgroundColor: 0xffffff,
-  scene: [LoadScene, MenuScene, HUDScene, GameScene],
+  scene: [],
   type: Phaser.AUTO,
   input: {
     keyboard: true
