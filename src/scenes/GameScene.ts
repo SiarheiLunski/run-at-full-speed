120e70278fcd3417de255ea8bdd83b113ba9cb81
Siarhei Lunski <loonskai@gmail.com>
2020-11-25 19:35:15 +0300

    Initialize scenes

--- src/scenes/GameScene.ts
+++ src/scenes/GameScene.ts
@@ -1,22 +1,14 @@
 import * as Phraser from 'phaser';
-import { SCENES, MAX_ENEMY_SPEED, MIN_ENEMY_SPEED } from '../constants';
-import { Player } from '../objects/Player';
-import { Enemy } from '../objects/Enemy';
+import { SCENES } from '../constants';
 
 export class GameScene extends Phraser.Scene {
-  private player: Player;
   private ground: Phaser.GameObjects.Sprite;
-  private enemies: Phaser.GameObjects.Group;
-  public spawnInterval: number;
 
   constructor() {
     super({ key: SCENES.GAME });
   }
 
-  init(): void {
-    Player.initAnimations(this);
-    Enemy.initAnimations(this);
-  }
+  init(): void { }
 
   create(): void {
     this.add.image(0, 0, 'background').setOrigin(0, 0);
@@ -24,55 +16,7 @@ export class GameScene extends Phraser.Scene {
     this.ground.scaleX = 2;
 
     this.physics.add.existing(this.ground, true);
-
-    this.player = new Player({
-      scene: this,
-      x: 100,
-      y: 300,
-      key: 'player'
-    });
-
-    this.enemies = this.add.group({ runChildUpdate: true });
-
-    this.spawnInterval = window.setInterval(() => {
-      this.enemies.add(new Enemy({
-        scene: this,
-        x: 800, 
-        y: 310, 
-        key: 'enemy',
-        speed: -(Math.random() * (MAX_ENEMY_SPEED - MIN_ENEMY_SPEED) + MIN_ENEMY_SPEED)
-      }));
-    }, 1500);
-
-    this.physics.add.collider(this.ground, this.player);
-    this.physics.add.collider(this.ground, this.enemies);
-
-    this.physics.add.overlap(
-      this.player,
-      this.enemies,
-      this.handlePlayerEnemyOverlap as any,
-      undefined,
-      this
-    );
-
-    this.events.on('shutdown', () => {
-      window.clearInterval(this.spawnInterval);
-    });
-    this.events.on('destroy', () => {
-      window.clearInterval(this.spawnInterval);
-    });
   }
 
-  update(): void {
-    this.player?.update();
-  }
-  
-  private handlePlayerEnemyOverlap(player: Player, enemy: Enemy): void {
-    if (player.body.touching.down && enemy.body.touching.up) {
-      player.bounceUpAfterHitEnemyOnHead();
-      enemy.gotHitOnHead();
-    } else if (player.isVulnerable) {
-      player.gotHit();
-    }
-  }
+  update(): void { }
 }
